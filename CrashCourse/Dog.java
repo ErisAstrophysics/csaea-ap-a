7460069fd88a6c8dd5a5f7bce2df440cb93bddaf
eris_astrophysics <shramek20288327@student.nhvweb.net>
2026-09-15 12:25:57 +0000

    Created Dog Class

---
+++ CrashCourse/Dog.java
@@ -0,0 +1,55 @@
+public class Dog {
+    
+    // Attributes
+    String name;
+    String ownerName;
+    String breed;
+    int age;
+    double weight;
+    boolean isHungry;
+    boolean isHome;
+    int energyLevel;
+
+    // Constructor
+    public Dog(String name, String ownerName, String breed) {
+        this.name = name;
+        this.ownerName = ownerName;
+        this.breed = breed;
+
+        energyLevel = 75;
+        weight = 35.4;
+        isHungry = false;
+        isHome = true;
+        age = 3;
+    }
+
+    // Behaviors
+    public void energy() {
+        if (energyLevel > 100){
+            energyLevel = 100;
+        }
+        if (energyLevel < 0){
+            energyLevel = 0;
+            isHungry = true;
+        }
+    }
+
+    public void feed() {
+        if (isHungry = true){
+            isHungry = false;
+            System.out.println("Your dog is full.")
+            weight += 1;
+        }
+        else{
+            System.out.println("Your dog is not hungry.");
+        }
+    }
+
+    
+
+
+
+
+
+
+}
