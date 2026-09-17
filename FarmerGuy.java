d99ac3892f27b0c64efa7cfb8fef10cdca93f30e
eris_astrophysics <shramek20288327@student.nhvweb.net>
2026-09-17 00:50:43 +0000

    OOP design activity 2 hw

--- FarmerGuy.java
+++ FarmerGuy.java
@@ -13,7 +13,6 @@ public class FarmerGuy {
     int pitchforksEarned;
     private double money;
     int farmVehiclesOwned;
-    int acresLand;
 
     // Constructor
     public FarmerGuy(String farmerName, String cropType, String gender){
@@ -24,7 +23,71 @@ public class FarmerGuy {
         energyLevel = 100;
         cropsHarvested = 0;
         pitchforksEarned = 0;
-        
+        money = 100;
+    }
+
+    public void energy() {
+        if (energyLevel < 0){
+            energyLevel = 0;
+        }
+        if (energyLevel > 100){
+            energyLevel = 100;
+        }
+    }
+   
+    public void harvestcrops() {
+        cropsHarvested += 15;
+        energyLevel -= 10;
+    }
+
+    public void sleep(int h = 0; h < 8; h++) {
+        System.out.println(h + " hours slept.");
+        energyLevel = 100;
+    }
+    
+    public void birthday() {
+        age += 1;
+    }
+
+    public void eat() {
+        energyLevel += 10;
+        isHungry = false;
+    }
+
+    public void buyvehicle() {
+        farmVehiclesOwned += 1;
+        money -= 50;
+    }
+
+    public void throwpicthfork() {
+        pitchforksEarned -= 1;
+        energyLevel -= 5;
+        if (pitchforksEarned <= 0){
+            System.out.println("You have no pitchsforks to throw.");
+            pitchforksEarned = 0;
+        }
+        else {
+            System.out.println("Pitchfork thrown.");
+            pitchforksEarned -= 1;
+        }
+    }
+
+    public void sellcrops() {
+        money += 25;
+    }
+
+    public void season() {
+        System.out.println("It is now: " + season);
+    }
+
+    public void checkbalance() {
+        if (money < 0){
+            System.out.println("Insufficient funds.");
+            money = 0;
+        }
+        else {
+            System.out.print("You have: " + money + " dollars.");
+        }
     }
 
 
