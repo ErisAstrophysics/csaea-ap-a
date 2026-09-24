f44c426761b0cd23abc13a259b580b4a03946d09
eris_astrophysics <shramek20288327@student.nhvweb.net>
2026-09-24 15:04:10 +0000

    Added LibraryBook class

---
+++ CrashCourse/LibraryBook.java
@@ -0,0 +1,108 @@
+public class LibraryBook {
+    
+    //Attributes
+    private String bookTitle;
+    private String authorName;
+    private String genre;   
+    private boolean isCheckedOut;
+    private boolean isDamaged;
+    private int numberPages;
+    private boolean isOnHold;
+    private double bookRating;
+
+    //Constructor
+    public LibraryBook(String bookTitle, String authorName, String genre, int numberPages){
+        this.bookTitle = bookTitle;
+        this.authorName = authorName;
+        this.genre = genre;
+        this.numberPages = numberPages;
+
+        isCheckedOut = false;
+        isDamaged = false;
+        isOnHold = false;
+        bookRating = 5.0;
+    }
+
+    //Methods
+    public void checkout(){
+        if(isCheckedOut = true) {
+            isCheckedOut = true;
+            System.out.println(bookTitle + " is currently unavailable to check out.");
+        }
+        else if(isOnHold = true){
+            isOnHold = true;
+            System.out.println("This title is currently on hold and cannot be checked out.");
+        }
+        else {
+            isCheckedOut = true;
+            System.out.println("You have checked out "  + bookTitle + ".");
+        }
+    }
+
+    public void booklength(){
+        System.out.println(bookTitle + " is " + numberPages + " pages long.");
+    }
+
+    public void whatgenre() {
+        System.out.println(bookTitle + " is " + genre + ".");
+    }
+
+    public void returnbook() {
+        if(isCheckedOut =  true){
+            isCheckedOut = false;
+            System.out.println(bookTitle +  " has been returned.");
+            System.out.println("Thank you for returning your book!");
+        }
+        else{
+            isCheckedOut = false;
+            System.out.println("This title was not checked out");
+        }
+    }
+
+    public void putonhold() {
+        if(isCheckedOut = true){
+            isOnHold = false;
+            System.out.println(bookTitle + " is currently checked out and cannot be put on hold.");
+        }
+        else {
+            isOnHold = true;
+            System.out.println(bookTitle + " has been put on hold.");
+        }
+    }
+
+    public void returnhold() {
+        isOnHold = false;
+        System.out.println("You have removed your hold on " + bookTitle + ".");
+    }
+
+    public void markdamaged() {
+        isDamaged = true;
+        System.out.println("Thank you for marking " + bookTitle + " as damaged. Our librarians will fix it soon.");
+    }
+
+    public void viewrating(){
+        System.out.print(bookTitle + " is rated " + bookRating + " out of 5.0.");
+    }
+
+    public void addrating(double addedRating){
+        if (addedRating > 5.0){
+            addedRating = 5.0;
+            System.out.println("Rating limit is 5.0.");
+            bookRating = (addedRating + 5.0) / 2;
+        }
+        else if (addedRating < 0.0){
+            addedRating = 0.0;
+            System.out.println("Rating limit is 0.0.");
+        }
+        else{
+            bookRating = (addedRating + 5.0) / 2;
+        }
+
+    }
+
+
+
+
+
+    
+}
